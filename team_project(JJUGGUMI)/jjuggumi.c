16ff0cdbd96af4838fc7b508f53423b09589f0c2
parktaejoo <parktaejoo1565@gmail.com>
2023-10-19 05:56:56 +0900

    mugunghwa.c 파일 추가

--- team_project(JJUGGUMI)/jjuggumi.c
+++ team_project(JJUGGUMI)/jjuggumi.c
@@ -1,4 +1,6 @@
 // 2023-2 고급프로그래밍 과제: 쭈꾸미 게임
+// 게임을 시작할 때 적당한 ascii art를 구현하는
+// intro() 작성 (5초 내로 끝내기)
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -6,6 +8,14 @@
 #include "jjuggumi.h"
 
 #define	DATA_FILE	"jjuggumi.dat"
+
+void intro()
+{
+	printf("======================================\n");
+	printf(" 쭈꾸미 게임에 오신 것을 환영합니다!\n");
+	printf("======================================\n");
+}
+
 
 int jjuggumi_init(void);
 
@@ -19,7 +29,7 @@ int randint(int low, int high) {
 int jjuggumi_init(void) {
 	srand((unsigned int)time(NULL));
 
-	printf("플레이어 수: ");
+	printf("\n플레이어 수: ");
 	scanf_s("%d", &n_player);
 
 	n_alive = n_player;
@@ -31,6 +41,7 @@ int jjuggumi_init(void) {
 
 int main(void) {
 	jjuggumi_init();
+	intro();
 	sample();
 	//mugunghwa();
 	//nightgame();
